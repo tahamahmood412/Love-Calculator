8441ed1cf461210829568e8fd3897d414aa53ab6
TM412 <tahamahmood412@gmail.com>
2023-12-24 22:33:46 +0500

    Updated

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var loveCalculator = function () {
 
     var name2 = document.getElementById("lovername").value;
     var loversName = name2.toLowerCase();
-    
+
     var loveScore = Math.random() * 100;
     var loveFinal = Math.round(loveScore);
 
@@ -20,7 +20,7 @@ var loveCalculator = function () {
         alert("Please Enter Your Name");
     } else if (loversName === "") {
         alert("Please Enter your lover's Name");
-    } else if ((yourName === "Taha" && loversName === "Aqsa") || (yourName === "Aqsa" && loversName === "Taha")) {
+    } else if ((yourName === "taha" && loversName === "aqsa") || (yourName === "aqsa" && loversName === "taha")) {
         document.getElementById("lovefinal").innerHTML = yourName + " and " + loversName + " your lovescore is " + 100 + " % ";
     } else if (isNotClicked) {
         document.getElementById("lovefinal").innerHTML = yourName + " and " + loversName + " your lovescore is " + loveFinal + " % ";
