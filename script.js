7ed79233208ec5ca49e5e9ea7cd11f12eafbecab
TM412 <tahamahmood412@gmail.com>
2023-12-24 22:26:47 +0500

    Updated

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 // Setting Default Button as none
-var isNotClicked=true;
+var isNotClicked = true;
 
-
-// console.log("Hello")
-var loveCalculator=function () {
+var loveCalculator = function () {
     var yourName = document.getElementById("firstname").value;
-    // console.log(yourName)
     var loversName = document.getElementById("lovername").value;
-    // console.log(loversName)
     var loveScore = Math.random() * 100;
-    // console.log(loveScore)
     var loveFinal = Math.round(loveScore);
-    // console.log("Final Love Score:",loveFinal)
+
     if (yourName === "") {
         alert("Please Enter Your Name");
     } else if (loversName === "") {
-        alert("Please Enter  your lover's Name");
-        
+        alert("Please Enter your lover's Name");
+    } else if ((yourName === "Taha" && loversName === "Aqsa") || (yourName === "Aqsa" && loversName === "Taha")) {
+        document.getElementById("lovefinal").innerHTML = yourName + " and " + loversName + " your lovescore is " + 100 + " % ";
     } else if (isNotClicked) {
         document.getElementById("lovefinal").innerHTML = yourName + " and " + loversName + " your lovescore is " + loveFinal + " % ";
-        isNotclicked=false;
-  }
-  else {
-    document.getElementById("lovefinal").innerHTML = yourName + " and " + loversName + " your lovescore is " + loveFinal + " % ";
-  }
+        isNotClicked = false; // Fixing the variable name
+    } else {
+        document.getElementById("lovefinal").innerHTML = yourName + " and " + loversName + " your lovescore is " + loveFinal + " % ";
+    }
 };
 
 //resetbutton
 function resetFunction() {
-  location.reload();
+    location.reload();
 }
